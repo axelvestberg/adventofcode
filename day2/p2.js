d3ab748801d87711121b56b66feadaf5d9e4f574
axelvestberg <axel.vestberg@gmail.com>
2019-12-06 11:06:25 +0100

    removed empty line

--- day2/p2.js
+++ day2/p2.js
@@ -1,22 +1,22 @@
 let fs = require("fs");
 
 const string = fs.readFileSync("./p1.input.txt", "utf-8");
-const state = string.split(',').map(Number)
+const state = string.split(",").map(Number);
 
 let noun = null;
 let verb = null;
 
-const opcode = (state) => {
+const opcode = state => {
   state[1] = 89;
   state[2] = 76;
 
-  for (let i = 0; i < state.length; i+=4) {
+  for (let i = 0; i < state.length; i += 4) {
     if (state[i] === 1) {
-      let sum = state[state[i+1]] + state[state[i+2]];
-      state[state[i+3]] = sum;
+      let sum = state[state[i + 1]] + state[state[i + 2]];
+      state[state[i + 3]] = sum;
     } else if (state[i] === 2) {
-      let sum = (state[state[i+1]] * state[state[i+2]]);
-      state[state[i+3]] = sum
+      let sum = state[state[i + 1]] * state[state[i + 2]];
+      state[state[i + 3]] = sum;
     } else {
       if (state[i] === 99) {
         console.log("exit program with state at pos 0", state[0]);
@@ -28,10 +28,9 @@ const opcode = (state) => {
         return;
       }
     }
-
   }
-}
+};
 
-opcode(state)
+opcode(state);
 
 // should output 19690720
